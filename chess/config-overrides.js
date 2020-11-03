72504ebb4495230e10547a236dece642455a835a
Michał Wieczorek <m98.wieczorek@onet.pl>
2020-11-03 17:22:16 +0100

    Add additional option playground section

--- chess/config-overrides.js
+++ chess/config-overrides.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 module.exports = override(
     addWebpackAlias({
-        '@global-components': path.resolve(__dirname, './src/utils/global-components')
+        '@global-components': path.resolve(__dirname, './src/utils/global-components'),
+        '@playground': path.resolve(__dirname, './src/game/playground')
     })
 )
--- chess/src/App.js
+++ chess/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import MainNavbar from './Navbar'
-import PlaygroundBar from './PlaygroundBar';
+import PlaygroundBar from './game/playground/PlaygroundBar';
 
 // import Badge from 'react-bootstrap/Badge'
 
