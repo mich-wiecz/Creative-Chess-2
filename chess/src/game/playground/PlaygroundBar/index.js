26def9a7c4359a5110b79eadeeefae44ca2497ba
Michał Wieczorek <m98.wieczorek@onet.pl>
2020-11-06 19:04:57 +0100

    Add buttons panel in playground

--- chess/src/game/playground/PlaygroundBar/index.js
+++ chess/src/game/playground/PlaygroundBar/index.js
@@ -6,11 +6,13 @@ import Col from 'react-bootstrap/Col';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Popover from 'react-bootstrap/Popover';
 import {ResizableBox} from 'react-resizable';
-import 'react-resizable/css/styles.css'
+import 'react-resizable/css/styles.css';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Button from 'react-bootstrap/Button';
 import classes from './PlaygroundBar.module.scss';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowsAlt, faChessKnight, faGamepad, faPalette, faPlusSquare, faMousePointer, faStar, faGripVertical } from '@fortawesome/free-solid-svg-icons';
+import { faArrowsAlt, faChessKnight, faGamepad, faPalette, faPlusSquare, faMousePointer, faStar, faStepBackward, faStepForward, faFire } from '@fortawesome/free-solid-svg-icons';
 
 import {AdditionalOptions} from '@playground/AdditionalOptions';
 import Help from '@playground/Help';
@@ -27,7 +29,7 @@ export default function PlaygroundBar() {
 
 
     const [showAllTabs, setShowingAllTabs] = useState(false);
-    const [activeKey, setActiveKey] = useState('');
+    const [activeKey, setActiveKey] = useState(null);
 
     function TabItem({ text, icon, eventKey, disabled = false }) {
 
@@ -43,7 +45,12 @@ export default function PlaygroundBar() {
             </Popover.Content>
           </Popover>}
         >
-          <Nav.Item className="py-2">
+          <Nav.Item 
+          className="py-2"
+          onClick={() => {
+             setActiveKey(activeKey === eventKey ?  null : eventKey);
+          }}
+          >
             <Nav.Link eventKey={eventKey} disabled={disabled}>
               <TabIcon icon={icon} />
             </Nav.Link>
@@ -51,9 +58,26 @@ export default function PlaygroundBar() {
         </OverlayTrigger>
       );
     }
-
     return (
-      <Tab.Container  id="playground-tabs">
+      <>
+      <ButtonGroup 
+      className="mt-3 mx-auto d-flex w-50"
+      aria-label="Podróże w czasie i przycisk rozpoczęcia gry">
+  <Button variant="light"  style={{width: "0px"}}>
+    <FontAwesomeIcon icon={faStepBackward}/>
+  </Button>
+  <Button variant="light"  style={{width: "0px"}}>
+    <FontAwesomeIcon icon={faStepForward}/>
+  </Button>
+  <Button className="bg-maroon w-75" >
+    <div className="d-flex justify-content-around">
+  <FontAwesomeIcon icon={faFire} size="lg" color="pink"/>
+    Rozpocznij grę 
+    <FontAwesomeIcon icon={faFire} size="lg" color="pink"/>
+    </div>
+    </Button>
+</ButtonGroup>
+      <Tab.Container id="playground-tabs">
       <Row >
         <Col 
         className="mt-5 ml-2  rounded"
@@ -104,45 +128,45 @@ export default function PlaygroundBar() {
         </Col>
       </Row>
       <ResizableBox
-          className={`react-resizable bg-primary text-light ${classes.Displayer}  p-4  overflow-auto rounded border-maroon`}
-          style={{width: 1000}}
-      handle={ (h) => 
-      <div
-        className={`react-resizable-handle react-resizable-handle-${h}`}
-        style={{
-          // transformOrigin: 'bottom',
-          // transform: 'scale(3)',
-          width: 60,
-          height: 60
-        }}
-        />
-      }
-      resizeHandles={[ 'se']}
-      axis="y"
-      minConstraints={[860, 200]}
-      maxConstraints={[860, 800]}
-      width={860}
-      height={400}
-      >
-        <Tab.Content >
-        <Tab.Pane eventKey="modes">
-          <ModesAndTimeSetter/>
-              </Tab.Pane>
-              <Tab.Pane eventKey="interaction-table">
-               <Help />
-              </Tab.Pane>
-              <Tab.Pane eventKey="additional">
-              <AdditionalOptions />
-              </Tab.Pane>
-              <Tab.Pane eventKey="color-motive">
-              <ColorMotivePanel />
-              </Tab.Pane>
-              <Tab.Pane eventKey="poles-resizing">
-              <BoardFieldResizer />
-              </Tab.Pane>
-             
-            </Tab.Content>
-        </ResizableBox>
+      className={`react-resizable bg-primary text-light ${classes.Displayer}  p-4  overflow-auto rounded border-maroon fade ${activeKey && "show"}`}
+      style={{width: 1000}}
+  handle={ (h) => 
+  <div
+    className={`react-resizable-handle react-resizable-handle-${h}`}
+    style={{
+      width: 60,
+      height: 60
+    }}
+    />
+  }
+  resizeHandles={[ 'se']}
+  axis="y"
+  minConstraints={[860, 200]}
+  maxConstraints={[860, 800]}
+  width={860}
+  height={400}
+  >
+    <Tab.Content >
+    <Tab.Pane eventKey="modes">
+      <ModesAndTimeSetter/>
+          </Tab.Pane>
+          <Tab.Pane eventKey="interaction-table">
+           <Help />
+          </Tab.Pane>
+          <Tab.Pane eventKey="additional">
+          <AdditionalOptions />
+          </Tab.Pane>
+          <Tab.Pane eventKey="color-motive">
+          <ColorMotivePanel />
+          </Tab.Pane>
+          <Tab.Pane eventKey="poles-resizing">
+          <BoardFieldResizer />
+          </Tab.Pane>
+         
+        </Tab.Content>
+    </ResizableBox>
+
     </Tab.Container>
+    </>
     )
 }
