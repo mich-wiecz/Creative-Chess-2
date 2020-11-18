93c1176020f96058000b01e9fabf81600862db9b
Michał Wieczorek <m98.wieczorek@onet.pl>
2020-11-18 11:51:42 +0100

    Add preserving state in local storage

--- chess/src/redux/store.js
+++ chess/src/redux/store.js
@@ -1,10 +1,20 @@
 import {configureStore} from '@reduxjs/toolkit';
 import chessReducer from 'redux/chessSlice';
 import userReducer from 'redux/userSlice';
+import {saveState, loadState} from './localStorage';
+import throttle from 'lodash.throttle'
 
-export default configureStore({
+const store =  configureStore({
     reducer: {
         chess: chessReducer,
         user: userReducer
-    }
+    },
+    preloadedState: loadState()
 })
+
+store.subscribe(throttle(() => {
+    saveState(store.getState())
+}), 1000, {leading: true, trailing: false})
+
+
+export default store;
