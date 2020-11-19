b5e71352784af7970033d4d675e257628630096e
Michał Wieczorek <m98.wieczorek@onet.pl>
2020-11-19 21:16:32 +0100

    Add loader to Rules

--- chess/src/Playground/Rules/index.js
+++ chess/src/Playground/Rules/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {rulesUrls} from 'data.json';
+import Spinner from 'react-bootstrap/Spinner';
 
 
 
-function IframeComponent ({onClick, mode, title})  {
-  return (
-<div
+export default function Rules({mode}) {
+
+
+  const [loading, setLoading] = useState(true)
+
+  const setModeTitle = () => {
+    switch(mode) {
+      case '960': return 'Zasady gry w szachy losowe (960)'
+      case 'capablanca': return 'Zasady gry w capablanki'
+      default: return 'Zasady gry w szachy klasyczne'
+    }
+  }
+
+
+
+
+  return <div
   style={{
     position: 'relative',
     width: '100%',
     height: '100%',
   }}
   >
+    {
+      loading &&
+      <div
+      style={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)'
+      }}
+      className="flex justify-content-center align-items-center"
+      >
+    <Spinner 
+    role="status"
+    animation="border"
+    aria-hidden="true"
+    style={{
+      width: 100,
+      height: 100
+    }}
+    />
+    </div>
+    }
+    
     {/* <FontAwesomeIcon 
     icon={faRedo}
     color="blue"
@@ -25,36 +63,15 @@ function IframeComponent ({onClick, mode, title})  {
     }}
     /> */}
     <iframe 
+    onLoad={() => setLoading(false)}
       id="rules-iframe"
     style={{
       width: '100%',
       height: '100%',
     }}
     src={rulesUrls[mode]} 
-    title={title}
+    title={setModeTitle()}
+    alt={"Hesdgafsgddfgdgfr"}
     />
     </div>
-  )
-  
-  }
-
-
-export default function Rules({mode}) {
-
-  const setModeTitle = () => {
-    switch(mode) {
-      case '960': return 'Zasady gry w szachy losowe (960)'
-      case 'capablanca': return 'Zasady gry w capablanki'
-      default: return 'Zasady gry w szachy klasyczne'
-    }
-  }
-
-
-
-
-  return <IframeComponent 
-  title={setModeTitle()}
-  mode={mode}
-
-   />;
 }
